eaccf120ebd6b27174953d73dc202b495d133fbc
nidhisn <nidhi24sn@gmail.com>
2024-12-02 01:15:37 +0530

    initial commit

---
+++ src/Components/Button/Button.jsx
@@ -0,0 +1,17 @@
+import styles from './Button.module.css'
+
+const Button = (props) => {
+
+  const { isOutline, icon, text } = props;
+
+  return (
+    <button className={isOutline ? styles.outline_btn : styles.primary_btn}>
+        
+        {props.icon}
+        {props.text}
+        
+        </button>
+  );
+};
+
+export default Button
